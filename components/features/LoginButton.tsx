be91e2ccb376d797ecd0e3c8e21ad3f350077841
JOEIH <joh73459@gmail.com>
2026-04-28 17:43:38 +0900

    Chore: 로그인 버튼에서 공용 버튼 컴포넌트 적용 취소

--- components/features/LoginButton.tsx
+++ components/features/LoginButton.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import Button from "@/components/common/Button";
-
 export default function LoginButton() {
   const handleLogin = () => {
     alert("로그인 버튼이 클릭되었습니다.");
   }
 
   return (
-    <Button text="로그인" bgColor="bg-brand-gray-50" textColor="text-brand-blue-700" width="w-[264px]" height="h-[52px]" padding="px-9 py-2.5" onClick={handleLogin} />
+    <button className="text-typo-title bg-brand-gray-50 text-brand-blue-700 border border-brand-blue-200 rounded-lg h-13 w-66" onClick={handleLogin}>
+      로그인
+    </button>
   )
 }
