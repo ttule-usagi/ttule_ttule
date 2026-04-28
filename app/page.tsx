85f9f10c8bc0f2c80e68a9585a2998c476cd3ddd
JOEIH <joh73459@gmail.com>
2026-04-28 17:44:49 +0900

    Design: 메인페이지 디자인 적용(1차) - 개발새발 제외한 아이템 이미지를 public 폴더에서 가져와서 적용 - public에 이미지용 폴더 추가 - 화면 사이즈 줄어들 때 아이템들이 안쪽으로 부드럽게 들어가도록 추가수정 필요

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,79 @@
-import LoginButton from "@/components/features/LoginButton";
+import JoinPlanButton from '@/components/features/JoinPlanButton';
+import LoginButton from '@/components/features/LoginButton';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <p>함께 떠나기 좋은 여행 플래너</p>
-      <p>뚤레뚤레</p>
-      <LoginButton />
+    <div className='bg-brand-blue-50 w-screen h-screen relative overflow-hidden'>
+      <div className='w-full h-full flex flex-col items-center justify-center'>
+        <div className='flex flex-col gap-2 items-center justify-center z-10 relative'>
+          <p className='text-typo-title text-brand-gray-200'>함께 떠나기 좋은 여행 플래너</p>
+          <Image
+            src='/images/logo.svg'
+            width={290}
+            height={94}
+            alt='Logo'
+          />
+          <Image
+            src='/images/clip.svg'
+            width={97.54}
+            height={29.31}
+            alt='clip'
+            className='absolute bottom-[-36%] right-[-32%]'
+          />
+        </div>
+        <div className='flex flex-col gap-2.5 z-10 mt-66'>
+          <JoinPlanButton />
+          <LoginButton />
+        </div>
+        <p className='text-white text-typo-description mt-3 z-10'>
+          처음 방문하셨나요?{' '}
+          <Link
+            href='/signup'
+            className='text-neon-green underline'
+          >
+            회원가입
+          </Link>
+        </p>
+      </div>
+
+      {/* 배경 아이템 */}
+      <Image
+        src='/images/bg_check_blue.svg'
+        width={1999}
+        height={1047}
+        alt='background check blue'
+        className='absolute w-screen h-auto top-[-18%] right-[-20%] rotate-[-6.36deg] z-1 blur-[2px]'
+      />
+      <Image
+        src='/images/clips.svg'
+        width={269.68}
+        height={351.24}
+        alt='clips'
+        className='absolute w-[14vw] h-auto top-[5%] left-[2%] z-1'
+      />
+      <Image
+        src='/images/pen.svg'
+        width={507.15}
+        height={769.01}
+        alt='pen'
+        className='absolute w-[26vw] h-auto rotate-[154.98deg] top-[-28%] left-[54%] z-1'
+      />
+      <Image
+        src='/images/pencil.svg'
+        width={710.38}
+        height={291.75}
+        alt='pencil'
+        className='absolute w-[40vw] h-auto bottom-[5%] left-[-17%] rotate-[-25.02deg] z-1'
+      />
+      <Image
+        src='/images/glasses.svg'
+        width={735.32}
+        height={588.83}
+        alt='glasses'
+        className='absolute w-[40vw] h-auto right-[3%] bottom-0 z-1'
+      />
     </div>
   );
 }
