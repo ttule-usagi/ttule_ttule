ecf744b36b2e77c0e4466349413bacf9098f4c0b
JOEIH <joh73459@gmail.com>
2026-04-28 17:43:06 +0900

    Design: 여행 계획 참여하기버튼 컴포넌트 생성

---
+++ components/features/JoinPlanButton.tsx
@@ -0,0 +1,7 @@
+export default function JoinPlanButton() {
+  return (
+    <button className="text-typo-title bg-neon-green text-brand-blue-700 rounded-lg h-13 w-66">
+      여행 계획에 참여하기
+    </button>
+  )
+}
