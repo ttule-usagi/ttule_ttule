340a956000538e4415603dbb7534ad38076a9c8d
JOEIH <joh73459@gmail.com>
2026-04-29 03:01:23 +0900

    Style: prettier 적용 및 폰트 같은 자잘한 설정

--- components/features/JoinPlanButton.tsx
+++ components/features/JoinPlanButton.tsx
@@ -1,7 +1,7 @@
 export default function JoinPlanButton() {
   return (
-    <button className="text-typo-title bg-neon-green text-brand-blue-700 rounded-lg h-13 w-66">
+    <button className='text-typo-title font-normal bg-neon-green text-brand-blue-700 rounded-lg h-13 w-66'>
       여행 계획에 참여하기
     </button>
-  )
+  );
 }
--- components/features/LoginButton.tsx
+++ components/features/LoginButton.tsx
@@ -2,12 +2,15 @@
 
 export default function LoginButton() {
   const handleLogin = () => {
-    alert("로그인 버튼이 클릭되었습니다.");
-  }
+    alert('로그인 버튼이 클릭되었습니다.');
+  };
 
   return (
-    <button className="text-typo-title bg-brand-gray-50 text-brand-blue-700 border border-brand-blue-200 rounded-lg h-13 w-66" onClick={handleLogin}>
+    <button
+      className='text-typo-title font-normal bg-brand-gray-50 text-brand-blue-700 border border-brand-blue-200 rounded-lg h-13 w-66'
+      onClick={handleLogin}
+    >
       로그인
     </button>
-  )
+  );
 }
