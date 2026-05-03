7073bc07171c51289683c9bd6eb4a8ef8ba41bb8
JOEIH <joh73459@gmail.com>
2026-05-03 17:22:21 +0900

    Chore: 사이드바 탭별 페이지 생성

---
+++ app/(lobby)/main/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className='h-screen w-screen'>로비창</div>;
+}
---
+++ app/(lobby)/places/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className='h-screen w-screen'>장소 리스트 페이지</div>;
+}
