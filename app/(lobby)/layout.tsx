f979d748c2898a02f11766ae7775e59a05305c4a
JOEIH <joh73459@gmail.com>
2026-05-03 17:32:19 +0900

    Design: 로비창 배경색 적용

--- app/(lobby)/layout.tsx
+++ app/(lobby)/layout.tsx
@@ -6,7 +6,7 @@ export default function LobbyLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className='h-screen w-screen flex'>
+    <div className='h-screen w-screen flex bg-brand-blue-50'>
       <Sidebar />
       <main className='flex-1'>{children}</main>
     </div>
