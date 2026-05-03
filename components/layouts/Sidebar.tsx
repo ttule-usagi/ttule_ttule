6ba58b7b378231993f5220f307b50d3530fe242d
JOEIH <joh73459@gmail.com>
2026-05-03 17:23:05 +0900

    Feat: 로비창, 장소리스트에서 사용할 사이드바 생성 - 탭별 아이콘 디자인 적용 - 클릭 시 현재 페이지 인식 후 페이지 이동

---
+++ components/layouts/Sidebar.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Icon } from '../common/Icon';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+const icons = [
+  { name: 'Luggage', href: '/main' },
+  { name: 'Bookmark', href: '/places' },
+];
+
+export default function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <div className='h-full max-w-16 bg-brand-blue-700 flex flex-col justify-center items-center gap-2 p-2'>
+      {icons.map((icon) => (
+        <Link
+          key={icon.name}
+          href={icon.href}
+        >
+          <button
+            className={`flex items-center justify-center p-2.5 rounded-lg ${pathname === icon.href ? 'bg-brand-blue-500 text-white' : 'text-brand-blue-300'}`}
+          >
+            <Icon
+              name={icon.name as 'Luggage' | 'Bookmark'}
+              size={28}
+            />
+          </button>
+        </Link>
+      ))}
+    </div>
+  );
+}
