579a8a45e676308c3371d5f2c17a01baec3b75f9
JOEIH <joh73459@gmail.com>
2026-05-04 20:49:34 +0900

    Feat: 여행 계획 참여 버튼 공용 컴포넌트화 - 메인페이지, 로비 페이지에 적용 - primary, secondary 로 디자인 각기 다르게 적용

--- components/features/JoinPlanButton.tsx
+++ components/features/JoinPlanButton.tsx
@@ -1,6 +1,17 @@
-export default function JoinPlanButton() {
+interface JoinPlanButtonProps {
+  variant: 'primary' | 'secondary';
+}
+
+const variantStyle = {
+  primary: 'text-typo-title px-9 py-2.5',
+  secondary: 'text-typo-base p-3',
+};
+
+export default function JoinPlanButton({ variant = 'primary' }: JoinPlanButtonProps) {
   return (
-    <button className='text-typo-title font-normal bg-neon-green text-brand-blue-700 rounded-lg h-13 w-66'>
+    <button
+      className={`font-normal rounded-lg bg-neon-green text-brand-blue-700 ${variantStyle[variant]} cursor-pointer`}
+    >
       여행 계획에 참여하기
     </button>
   );
