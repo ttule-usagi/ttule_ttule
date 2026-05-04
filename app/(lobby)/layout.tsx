f96a4e2606396b9f1cf577270b68c5823c4b85cf
JOEIH <joh73459@gmail.com>
2026-05-04 20:51:51 +0900

    Feat: 로비창에 적용되는 레이아웃 생성 및 설정 - 사이드바 크기에 따라 좌측 패딩값 설정 - 현재 /lobby, /places 페이지에만 적용됨(사이드바 아이콘에 해당되는 페이지)

--- app/(lobby)/layout.tsx
+++ app/(lobby)/layout.tsx
@@ -6,9 +6,9 @@ export default function LobbyLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className='h-screen w-screen flex bg-brand-blue-50'>
+    <div className='h-full w-full flex bg-brand-gray-50'>
       <Sidebar />
-      <main className='flex-1'>{children}</main>
+      <main className='flex-1 pl-32 pr-16 pb-60'>{children}</main>
     </div>
   );
 }
