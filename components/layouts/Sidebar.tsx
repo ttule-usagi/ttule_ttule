a4f2ab5304180a0db3fa4c29e56e7a335f1d0a20
JOEIH <joh73459@gmail.com>
2026-05-04 21:36:11 +0900

    Design: 사이드바 프로필 UI 디자인 기초세팅

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -3,6 +3,7 @@
 import { Icon } from '../common/Icon';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import ProfileImage from '../features/ProfileImage';
 
 const icons = [
   { name: 'Luggage', href: '/lobby' },
@@ -13,22 +14,28 @@ export default function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <div className='fixed h-full max-w-16 bg-brand-blue-700 flex flex-col justify-center items-center gap-2 p-2'>
-      {icons.map((icon) => (
-        <Link
-          key={icon.name}
-          href={icon.href}
-        >
-          <button
-            className={`flex items-center justify-center p-2.5 rounded-lg ${pathname === icon.href ? 'bg-brand-blue-500 text-white' : 'text-brand-blue-300'}`}
+    <div className='fixed h-full max-w-16 bg-brand-blue-700 flex flex-col justify-center items-center'>
+      <div className='flex flex-col justify-center gap-2 p-2'>
+        {icons.map((icon) => (
+          <Link
+            key={icon.name}
+            href={icon.href}
           >
-            <Icon
-              name={icon.name as 'Luggage' | 'Bookmark'}
-              size={28}
-            />
-          </button>
-        </Link>
-      ))}
+            <button
+              className={`flex items-center justify-center p-2.5 rounded-lg ${pathname === icon.href ? 'bg-brand-blue-500 text-white' : 'text-brand-blue-300'}`}
+            >
+              <Icon
+                name={icon.name as 'Luggage' | 'Bookmark'}
+                size={28}
+              />
+            </button>
+          </Link>
+        ))}
+      </div>
+
+      <div className='absolute bottom-5.5'>
+        <ProfileImage />
+      </div>
     </div>
   );
 }
