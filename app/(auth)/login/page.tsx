a6fec41727bb7afcd89f34cf93e2ec23d2e4501f
toma <verumnium@gmail.com>
2026-05-06 02:26:59 +0900

    chore: 개발에 사용한 콘솔로그 제거

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,6 @@ import { signInWithGoogle } from '@/lib/api/auth';
 import GoogleLoginButton from '@/components/features/GoogleLoginButton';
 
 export default function Login() {
-  console.log(process.env.SUPABASE_SERVICE_ROLE_KEY);
   return (
     <div>
       <GoogleLoginButton
