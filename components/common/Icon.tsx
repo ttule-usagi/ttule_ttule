b9b797a50f6ac8a6c61cce336f1bdd916f98dfae
JOEIH <joh73459@gmail.com>
2026-05-06 03:49:31 +0900

    Chore: 모달 아이콘 추가

--- components/common/Icon.tsx
+++ components/common/Icon.tsx
@@ -5,6 +5,9 @@ import DotsHorizontal from '@/assets/icons/dots-horizontal.svg';
 import Calendar from '@/assets/icons/calendar.svg';
 import Clock from '@/assets/icons/clock.svg';
 import ArrowRight from '@/assets/icons/arrow-right.svg';
+import Copy from '@/assets/icons/copy.svg';
+import Link from '@/assets/icons/link.svg';
+import XClose from '@/assets/icons/x-close.svg';
 
 export const ICONS = {
   Luggage,
@@ -14,6 +17,9 @@ export const ICONS = {
   Calendar,
   Clock,
   ArrowRight,
+  Copy,
+  Link,
+  XClose,
 } as const;
 
 export type IconName = keyof typeof ICONS;
