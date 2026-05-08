4e0a260992492a3883e8088bd298471da8682038
JOEIH <joh73459@gmail.com>
2026-05-08 21:17:29 +0900

    Feat: 초대 링크 입력 모달 수정

--- components/features/Plan/EnterInviteLinkModal.tsx
+++ components/features/Plan/EnterInviteLinkModal.tsx
@@ -1,25 +1,24 @@
-import { ShareLinkBaseModal } from '@/components/common/ShareLinkBaseModal';
-import { useModalStore } from '@/lib/store/modalStore';
+import ModalBox from '@/components/common/Modal/ModalBox';
 
 export default function EnterInviteLinkModal() {
-  const { close } = useModalStore();
   return (
-    <ShareLinkBaseModal
-      title='초대 링크 입력하기'
-      description='초대받은 여행 계획의 공유 링크를 입력해주세요'
-      onClose={close}
-    >
+    <ModalBox>
+      <ModalBox.ModalTitle
+        title='초대 링크 입력하기'
+        description='초대받은 여행 계획의 공유 링크를 입력해주세요'
+      />
+
       <div className='flex flex-col gap-1'>
         <p className='text-typo-caption text-brand-gray-400'>초대 링크</p>
 
-        <div className='flex gap-2.5'>
+        <ModalBox.ModalBottomContent>
           <input
             className='modal-input'
             placeholder='링크 입력하기'
           />
           <button className='modal-button'>확인</button>
-        </div>
+        </ModalBox.ModalBottomContent>
       </div>
-    </ShareLinkBaseModal>
+    </ModalBox>
   );
 }
