96dac38b8f733a11ac7b475baba2f8a7542fcb5d
JOEIH <joh73459@gmail.com>
2026-05-12 04:47:36 +0900

    Feat: 회원가입용 input에서 에러텍스트 받도록 변경

--- components/common/WithoutLineInput.tsx
+++ components/common/WithoutLineInput.tsx
@@ -1,19 +1,34 @@
 import { FormInputProps } from '@/types/types';
 
-export default function WithoutLineInput({ id, label, type = 'text', placeholder, value, onChange }: FormInputProps) {
+interface WithoutLineInputProps extends FormInputProps {
+  errorText: string;
+}
+
+export default function WithoutLineInput({
+  id,
+  label,
+  type = 'text',
+  placeholder,
+  value,
+  onChange,
+  errorText,
+}: WithoutLineInputProps) {
   return (
-    <div className='w-full flex gap-3 py-2 text-typo-base font-light min-w-87.5'>
-      <label htmlFor={id}>
-        <div className='text-brand-blue-700 w-16'>{`${label} :`}</div>
-      </label>
-      <input
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className='w-full max-h-6 border-none focus:outline-none placeholder:text-brand-gray-400'
-      />
+    <div className='flex flex-col'>
+      <div className='w-full flex gap-3 py-2 text-typo-base font-light min-w-87.5'>
+        <label htmlFor={id}>
+          <div className='text-brand-blue-700 w-16'>{`${label} :`}</div>
+        </label>
+        <input
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className='w-full max-h-6 border-none focus:outline-none placeholder:text-brand-gray-400'
+        />
+      </div>
+      {errorText && <p className='text-left w-full mt-1 text-typo-caption text-tag-red-text'>{errorText}</p>}
     </div>
   );
 }
