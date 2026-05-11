cd8a5142c416541bd088e450d2ccba347bc5da7e
JOEIH <joh73459@gmail.com>
2026-05-12 04:50:18 +0900

    Design: 모달 스타일 수정 - 공용 모달박스가 다양한 박스 너비나 아이콘 유무를 받을 수 있도록 수정

--- components/common/Modal/ModalBox.tsx
+++ components/common/Modal/ModalBox.tsx
@@ -7,21 +7,26 @@ import { useModalStore } from '@/lib/store/modalStore';
 
 interface ModalBoxProps {
   children: React.ReactNode;
+  isCloseIcon?: boolean;
+  width?: number;
 }
 
-export default function ModalBox({ children }: ModalBoxProps) {
+export default function ModalBox({ children, isCloseIcon = true, width = 560 }: ModalBoxProps) {
   const { close } = useModalStore();
   return (
     <div
-      className='flex flex-col gap-5 bg-white aspect-560/191 rounded-lg relative pt-7 px-6 pb-6 w-[clamp(380px,35vw,560px)]'
+      className='flex flex-col gap-5 bg-white aspect-560/191 rounded-lg relative pt-7 px-6 pb-6'
+      style={{ width: `clamp(320px,35vw,${width}px)` }}
       onClick={(e) => e.stopPropagation()}
     >
-      <Icon
-        name='XClose'
-        size={32}
-        className='absolute top-3 right-3 text-brand-gray-500 cursor-pointer'
-        onClick={close}
-      />
+      {isCloseIcon && (
+        <Icon
+          name='XClose'
+          size={32}
+          className='absolute top-3 right-3 text-brand-gray-500 cursor-pointer'
+          onClick={close}
+        />
+      )}
       {children}
     </div>
   );
