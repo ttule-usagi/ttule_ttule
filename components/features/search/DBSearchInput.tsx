2eba3833ae3ca01d43aac5f2ad2fd34772b9d1a1
JOEIH <joh73459@gmail.com>
2026-05-24 04:09:51 +0900

    design: db 장소검색 input 컴포넌트 생성 - 항상 상단에 떠 있으므로 places 라우트 그룹 layout.tsx에 위치 - 컴포넌트 내에서 현재 위치 감지하고 뒤로가기 버튼 생성 유무 결정

---
+++ components/features/search/DBSearchInput.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Icon } from '@/components/common/Icon';
+import { usePathname, useRouter } from 'next/navigation';
+
+export default function DBSearchInput() {
+  const path = usePathname();
+  const router = useRouter();
+
+  return (
+    <div className='flex gap-4 items-center'>
+      {path !== '/places' && (
+        <Icon
+          name='ArrowLeft'
+          size={32}
+          className='text-brand-blue-700 cursor-pointer'
+          onClick={() => router.back()}
+        />
+      )}
+      <div className='w-full flex text-typo-base border-brand-blue-700 pl-6 pr-4 py-1.25 bg-brand-gray-0 rounded-[30px] border-2 items-center flex-1 gap-2'>
+        <input
+          placeholder='장소 검색'
+          className='text-brand-gray-600 w-full outline-none'
+        />
+        <Icon
+          name='Search'
+          size={32}
+          className='text-brand-blue-700'
+        />
+      </div>
+    </div>
+  );
+}
