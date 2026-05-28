573b1932977faa9b5cde20bf6d7181c5a9c8225a
JOEIH <joh73459@gmail.com>
2026-05-29 04:45:46 +0900

    refactor: modal이 타입과 props를 함께 받을 때 타입 안정성이 보장되도록 수정

--- lib/store/modalStore.ts
+++ lib/store/modalStore.ts
@@ -3,25 +3,27 @@ import { create } from 'zustand';
 /**
  * 모달의 상태를 관리하는 Zustand 스토어
  *
- * - modal: 현재 열려있는 모달의 종류 (ModalType) 또는 null (모달 닫힘)
- * - open: 모달을 여는 함수, ModalType을 인자로 받아 modal 상태를 해당 타입으로 설정
+ * - activeModal: 현재 열려있는 모달의 유니온 타입
+ * - open: 모달을 여는 함수, open({type: 모달 타입, props: {prop1: '' ...}}) 와 같이 모달을 여는 곳에서 사용해줍니다.
  * - close: 모달을 닫는 함수, modal 상태를 null로 설정
  *
- * ModalType은 현재 'editLink', 'viewLink', 'enterInviteLink', 'confirm', 'cancelSignup' 다섯 가지 타입이 정의되어 있으며, 필요에 따라 새로운 모달 타입을 추가할 수 있음
+ * ModalPayload에 필요에 따라 새로운 모달 타입과 props를 추가할 수 있음
  */
 
-type ModalType = 'editLink' | 'viewLink' | 'enterInviteLink' | 'confirm' | 'cancelSignup' | 'cancelNewPlace';
+type ModalPayload =
+  | { type: 'enterInviteLink' | 'cancelSignup' } // props가 필요 없는 모달
+  | { type: 'editLink'; props: { link: string } }
+  | { type: 'viewLink'; props: { link: string } }
+  | { type: 'cancelNewPlace'; props: { onCancel: () => void } };
 
 interface ModalState {
-  modal: ModalType | null;
-  props: Record<string, any> | null | undefined;
-  open: (type: ModalType, props?: Record<string, any>) => void;
+  activeModal: ModalPayload | null;
+  open: (payload: ModalPayload) => void;
   close: () => void;
 }
 
 export const useModalStore = create<ModalState>((set) => ({
-  modal: null,
-  props: undefined,
-  open: (type, props) => set({ modal: type, props }),
-  close: () => set({ modal: null, props: null }),
+  activeModal: null,
+  open: (payload) => set({ activeModal: payload }),
+  close: () => set({ activeModal: null }),
 }));
