e97ec7ebf50c4394f601675d98232675dcb30ec2
JOEIH <joh73459@gmail.com>
2026-06-24 03:19:04 +0900

    feat: 장소 리스트에 저장된 장소 조회 queryOptions와 useSuspenseInfiniteQuery 훅 작성

--- hooks/place-list/useGetPlaceListDetail.ts
+++ hooks/place-list/useGetPlaceListDetail.ts
@@ -1,7 +1,9 @@
-import { getPlaceListDetail } from '@/lib/actions/placeList';
-import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+import { getPlaceListDetail, getPlaceListPlaces } from '@/lib/actions/placeList';
+import { PageParam } from '@/types/placeList';
+import { infiniteQueryOptions, queryOptions, useSuspenseInfiniteQuery, useSuspenseQuery } from '@tanstack/react-query';
 
 // queryOptions 정의 - 서버/클라이언트 캐시 동기화를 위해
+// 리스트 개요 queryOptions
 export const placeListDetailQueryOptions = (listId: string) => {
   return queryOptions({
     queryKey: ['place-list', listId, 'detail'],
@@ -9,6 +11,22 @@ export const placeListDetailQueryOptions = (listId: string) => {
   });
 };
 
+// 리스트에 저장된 장소 queryOptions
+export const placeListPlacesQueryOptions = (listId: string) => {
+  return infiniteQueryOptions({
+    queryKey: ['place-list', listId, 'places'],
+    queryFn: ({ pageParam }) => getPlaceListPlaces(listId, pageParam),
+    initialPageParam: null as PageParam,
+    getNextPageParam: (lastPage) =>
+      lastPage.length < 20 ? undefined : { createdAt: lastPage.at(-1)!.createdAt, id: lastPage.at(-1)!.id },
+  });
+};
+
 export const useGetPlaceListDetail = (listId: string) => {
   return useSuspenseQuery(placeListDetailQueryOptions(listId));
+};
+
+export const useGetPlaceListPlaces = (listId: string) => {
+  const { data, ...rest } = useSuspenseInfiniteQuery(placeListPlacesQueryOptions(listId));
+  return { data: data.pages.flat(), ...rest };
 };
