e3efa1ed074ac6eb2af2c485b3ce9d5580634f11
JOEIH <joh73459@gmail.com>
2026-06-24 03:18:32 +0900

    feat: 장소 리스트에 저장된 장소 호출 함수 작성 - 커서 기반 무한스크롤 - 20개씩 호출

--- lib/actions/placeList.ts
+++ lib/actions/placeList.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { AllPlaceLists, ListType, PlaceListDetail } from '@/types/placeList';
+import { AllPlaceLists, ListType, PageParam, Place, PlaceListDetail } from '@/types/placeList';
 import { auth } from '../utils/auth';
 import { supabaseAdmin } from '../utils/supabase';
 import { toCamelKey } from '../utils/toCamelCase';
@@ -66,7 +66,7 @@ export const getAllPlaceLists = async ({
   return toCamelKey<AllPlaceLists>(data);
 };
 
-// 장소 리스트 상세
+// 장소 리스트 상세 개요
 export const getPlaceListDetail = async (listId: string): Promise<PlaceListDetail> => {
   // 인증 확인
   const session = await auth();
@@ -84,4 +84,25 @@ export const getPlaceListDetail = async (listId: string): Promise<PlaceListDetai
   if (error) throw error;
   if (!data) throw new Error('장소 리스트 상세정보를 가져오는 데 실패했습니다.');
   return toCamelKey<PlaceListDetail>(data);
+};
+
+// 장소 리스트에 저장된 장소
+export const getPlaceListPlaces = async (listId: string, cursor: PageParam = null): Promise<Place[]> => {
+  // 인증 확인
+  const session = await auth();
+  if (!session?.user?.id) {
+    throw new Error('인증 정보가 없습니다.');
+  }
+
+  const supabase = await supabaseAdmin;
+  const { data, error } = await supabase.rpc('get_place_list_places', {
+    p_list_id: listId,
+    p_cursor_created_at: cursor?.createdAt ?? null,
+    p_cursor_id: cursor?.id ?? null,
+    p_limit: 20,
+  });
+
+  if (error) throw error;
+  if (!data) throw new Error('저장된 장소를 가져오는 데 실패했습니다.');
+  return toCamelKey<Place[]>(data);
 };
