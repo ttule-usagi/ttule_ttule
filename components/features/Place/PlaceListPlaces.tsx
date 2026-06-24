6eece3bc427a81f207ab9fb12c1cb253f67b28c6
JOEIH <joh73459@gmail.com>
2026-06-24 16:41:08 +0900

    refactor: 저장된 장소가 없을 때 EmptyState 반환

--- components/features/Place/PlaceListPlaces.tsx
+++ components/features/Place/PlaceListPlaces.tsx
@@ -2,9 +2,12 @@
 
 import { useGetPlaceListPlaces } from '@/hooks/place-list/useGetPlaceListDetail';
 import PlaceItem from './PlaceItem';
+import EmptyState from './EmptyState';
 
 export default function PlaceListPlaces({ listId }: { listId: string }) {
   const { data } = useGetPlaceListPlaces(listId);
+
+  if (data.length === 0) return <EmptyState message='저장된 장소가 아직 없습니다.' />;
 
   return (
     <>
