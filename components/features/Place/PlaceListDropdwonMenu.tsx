a0fdaa85a7d97084a51a00f3c33c5585f3a6fec5
JOEIH <joh73459@gmail.com>
2026-07-01 21:56:30 +0900

    refactor: 드롭다운 메뉴 아이템에 disabled 처리 추가

--- components/features/Place/PlaceListDropdwonMenu.tsx
+++ components/features/Place/PlaceListDropdwonMenu.tsx
@@ -7,7 +7,7 @@ import { useModalStore } from '@/lib/store/modalStore';
 
 export default function PlaceListDropdownMenu({ id }: { id: string }) {
   const { open } = useModalStore();
-  const { createShareLink } = useShareEditLink();
+  const { createShareLink, isPending } = useShareEditLink();
 
   return (
     <DropDown>
@@ -27,7 +27,12 @@ export default function PlaceListDropdownMenu({ id }: { id: string }) {
         >
           리스트를 보기 위한 링크 보내기
         </DropDown.Item>
-        <DropDown.Item onClick={() => createShareLink(id, 'place_list')}>수정할 수 있도록 초대</DropDown.Item>
+        <DropDown.Item
+          disabled={isPending}
+          onClick={() => createShareLink(id, 'place_list')}
+        >
+          수정할 수 있도록 초대
+        </DropDown.Item>
         <DropDown.Item>공유 옵션 관리</DropDown.Item>
         <DropDown.Item onClick={() => console.log('전체 리스트 편집 페이지로 이동')}>리스트 편집</DropDown.Item>
         <DropDown.Item>리스트 삭제</DropDown.Item>
