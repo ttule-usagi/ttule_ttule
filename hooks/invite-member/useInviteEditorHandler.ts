26dea09f6ecdc6bd5167f0fd4b85e0d5fe203ef9
JOEIH <joh73459@gmail.com>
2026-07-01 20:02:40 +0900

    feat: 초대 링크로 접속 후, 참여에 실패하면 에러 모달 노출

--- hooks/invite-member/useInviteEditorHandler.ts
+++ hooks/invite-member/useInviteEditorHandler.ts
@@ -1,21 +1,23 @@
-import { ResourceType } from '@/lib/actions/invite';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useAddEditMember } from './useAddEditMember';
 import { useEffect } from 'react';
+import { useModalStore } from '@/lib/store/modalStore';
+import { ResourceType } from '@/types/invite';
 
 // 라우터를 감지하고 참여 유저(editor)로 추가하는 훅
-export const useInviteEditorHandler = (type: ResourceType) => {
+export const useInviteEditorHandler = (id: string, type: ResourceType) => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
   const { mutate: addEditMember } = useAddEditMember();
+  const { open } = useModalStore();
 
   useEffect(() => {
     const token = searchParams.get('invite_token');
     if (!token) return;
 
     addEditMember(
-      { token, type },
+      { token, id, type },
       {
         // 토큰 확인되면 토큰 파라미터 제거하고 순수한 링크로 접속
         onSuccess: () => {
@@ -25,6 +27,13 @@ export const useInviteEditorHandler = (type: ResourceType) => {
           // 에러 모달
           console.error(error);
           router.replace(pathname);
+          open({
+            type: 'error',
+            props: {
+              title: `${type === 'place_list' ? '장소 리스트' : '계획'} 참여 실패`,
+              description: '초대 링크를 다시 확인해주세요.',
+            },
+          });
         },
       },
     );
