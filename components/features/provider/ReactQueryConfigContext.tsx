2776c26e61a56c37164e9986014951dea415bbe6
JOEIH <joh73459@gmail.com>
2026-07-02 12:33:23 +0900

    refactor: QueryClient App Router 패턴 전환 및 인스턴스 생성 로직 분리

--- components/features/provider/ReactQueryConfigContext.tsx
+++ components/features/provider/ReactQueryConfigContext.tsx
@@ -1,12 +1,12 @@
 'use client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
+import { getQueryClient } from '@/lib/utils/getQueryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 type Props = {
   children: React.ReactNode;
 };
 
 export default function ReactQueryConfigContext({ children }: Props) {
-  const [queryClient] = useState(() => new QueryClient());
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
