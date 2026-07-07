a9d67ee66d9101095382ebf720fbbf170ed17268
JOEIH <joh73459@gmail.com>
2026-07-07 10:41:20 +0900

    chore: lobby 페이지 계획 목록 조회 적용 - 간단한 정보 조회와 클릭 시 이동만 구현 - TODO: 추후 prefetch 적용 예정

--- app/(lobby)/lobby/page.tsx
+++ app/(lobby)/lobby/page.tsx
@@ -1,99 +1,21 @@
+// TODO: 공유 작업 끝나고 서버 컴포넌트로 전환 예정
+'use client';
+
 import JoinPlanButton from '@/components/features/JoinPlanButton';
 import LobbyLastPlanItem from '@/components/features/Plan/LobbyLastPlanItem';
 import LobbyPlanItem from '@/components/features/Plan/LobbyPlanItem';
 import NoticeHeader from '@/components/features/NoticeHeader';
 import Image from 'next/image';
 import Link from 'next/link';
-
-// 임시 데이터 - 추후 API 연동 시 제거 예정
-const PlanItemList = [
-  {
-    id: 1,
-    destination: 'KOR',
-    departure: '2024-07-01',
-    arrival: '2024-07-05',
-    planName: '국내 여행 계획',
-    party: 'group',
-    updatedAt: '2min ago',
-  },
-  {
-    id: 2,
-    destination: 'JPN',
-    departure: '2024-08-10',
-    arrival: '2024-08-15',
-    planName: '일본 여행 계획',
-    party: 'single',
-    updatedAt: '5min ago',
-  },
-  {
-    id: 3,
-    destination: 'USA',
-    departure: '2024-05-01',
-    arrival: '2024-05-10',
-    planName: '미국 여행 계획',
-    party: 'group',
-    updatedAt: '1분 전 수정',
-  },
-  {
-    id: 4,
-    destination: 'FRA',
-    departure: '2024-06-01',
-    arrival: '2024-06-10',
-    planName: '프랑스 여행 계획',
-    party: 'single',
-    updatedAt: '3분 전 수정',
-  },
-];
-
-const LastPlanItemList = [
-  {
-    id: 3,
-    destination: 'USA',
-    departure: '2024-05-01',
-    arrival: '2024-05-10',
-    planName: '미국 여행 계획',
-    party: 'group',
-    updatedAt: '1분 전 수정',
-  },
-  {
-    id: 4,
-    destination: 'FRA',
-    departure: '2024-06-01',
-    arrival: '2024-06-10',
-    planName: '프랑스 여행 계획',
-    party: 'single',
-    updatedAt: '3분 전 수정',
-  },
-  {
-    id: 5,
-    destination: 'ITA',
-    departure: '2024-04-01',
-    arrival: '2024-04-10',
-    planName: '이탈리아 여행 계획',
-    party: 'group',
-    updatedAt: '5분 전 수정',
-  },
-  {
-    id: 6,
-    destination: 'ESP',
-    departure: '2024-03-01',
-    arrival: '2024-03-10',
-    planName: '스페인 여행 계획',
-    party: 'single',
-    updatedAt: '10분 전 수정',
-  },
-  {
-    id: 7,
-    destination: 'GER',
-    departure: '2024-02-01',
-    arrival: '2024-02-10',
-    planName: '독일 여행 계획',
-    party: 'group',
-    updatedAt: '15분 전 수정',
-  },
-];
+import { useGetUserPlans } from '@/hooks/plan/useGetUserPlans';
+import { getTodayDateStr } from '@/lib/utils/date';
 
 export default function Page() {
+  const { data } = useGetUserPlans();
+  const lastPlans = data?.filter(
+    (item) => !item.isDateUndecided && item.arrivalDate && item.arrivalDate < getTodayDateStr(),
+  );
+
   return (
     <div className='h-full max-w-350 min-w-230 mx-auto mt-5.5 pl-16 pr-16 pb-60'>
       <NoticeHeader />
@@ -114,14 +36,15 @@ export default function Page() {
             다가오는 여행
           </div>
           <div className='grid grid-cols-[repeat(auto-fill,275.76px)] gap-10.75'>
-            {PlanItemList.map((plan, index) => (
-              <div
-                key={plan.id}
-                className={index % 2 !== 0 ? 'rotate-2' : '-rotate-2'}
-              >
-                <LobbyPlanItem {...plan} />
-              </div>
-            ))}
+            {data &&
+              data.map((plan, index) => (
+                <div
+                  key={plan.id}
+                  className={index % 2 !== 0 ? 'rotate-2' : '-rotate-2'}
+                >
+                  <LobbyPlanItem {...plan} />
+                </div>
+              ))}
             <Link href='/plan-create'>
               <Image
                 src='/images/lobby-create-plan.svg'
@@ -134,20 +57,22 @@ export default function Page() {
           </div>
         </div>
 
-        <div className='flex flex-col gap-6'>
-          <div className='flex items-center gap-4.5 text-typo-title text-brand-blue-800 font-medium'>
-            <div className='w-2.5 h-2.5 bg-brand-blue-700'></div>
-            지난 여행
+        {lastPlans && (
+          <div className='flex flex-col gap-6'>
+            <div className='flex items-center gap-4.5 text-typo-title text-brand-blue-800 font-medium'>
+              <div className='w-2.5 h-2.5 bg-brand-blue-700'></div>
+              지난 여행
+            </div>
+            <div className='grid grid-cols-[repeat(auto-fill,340px)] gap-3.25'>
+              {lastPlans.map((plan) => (
+                <LobbyLastPlanItem
+                  key={plan.id}
+                  {...plan}
+                />
+              ))}
+            </div>
           </div>
-          <div className='grid grid-cols-[repeat(auto-fill,340px)] gap-3.25'>
-            {LastPlanItemList.map((plan) => (
-              <LobbyLastPlanItem
-                key={plan.id}
-                {...plan}
-              />
-            ))}
-          </div>
-        </div>
+        )}
       </main>
     </div>
   );
