8b54b32a2d8e0315b9944f5a6dc1364256046895
JOEIH <joh73459@gmail.com>
2026-07-13 16:44:16 +0900

    fix: 사이드바 유지 결정에 따라 layout에서 main의 패딩값을 복구

--- app/(lobby)/layout.tsx
+++ app/(lobby)/layout.tsx
@@ -1,17 +1,14 @@
-import Sidebar from '@/components/layouts/Sidebar';
-import { auth } from '@/lib/utils/auth';
+import Sidebar from '@/components/layouts/sidebar/Sidebar';
 
 export default async function LobbyLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
-
   return (
     <div className='h-full w-full flex bg-brand-gray-50'>
-      {session && <Sidebar />}
-      <main className='flex-1'>{children}</main>
+      <Sidebar />
+      <main className='flex-1 pl-16'>{children}</main>
     </div>
   );
 }
