a405a0be34f426abbad5c151e37906c18963580f
JOEIH <joh73459@gmail.com>
2026-07-15 00:38:55 +0900

    chore: 공용 공유 옵션 모달 타입 정의

---
+++ types/shareOption.ts
@@ -0,0 +1,23 @@
+import { ResourceType } from './invite';
+
+export type Role = 'master' | 'editor';
+
+export interface ResourceParams {
+  id: string;
+  resourceType: ResourceType;
+}
+
+export interface SetPublicParams extends ResourceParams {
+  isPublic: boolean;
+}
+
+export interface DeleteMemberParams extends ResourceParams {
+  targetUserId: string;
+}
+
+export interface Member {
+  id: string;
+  username: string;
+  ProfileImageUrl: string;
+  role: Role;
+}
