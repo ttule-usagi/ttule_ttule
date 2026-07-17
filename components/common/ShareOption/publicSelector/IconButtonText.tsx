de1adf8ae732e62c9a23aeda79625cb38b77d01e
JOEIH <joh73459@gmail.com>
2026-07-17 18:04:58 +0900

    feat: 아이콘을 포함한 공개/비공개 버튼 컴포넌트 생성

---
+++ components/common/ShareOption/publicSelector/IconButtonText.tsx
@@ -0,0 +1,41 @@
+import { Icon, IconName } from '../../Icon';
+
+interface IconTextButtonProps {
+  iconName: IconName;
+  isSelected?: boolean;
+  disabled?: boolean;
+  onClick: () => void;
+  buttonText: string;
+  description: string;
+}
+
+export function IconButtonText({
+  iconName,
+  isSelected,
+  disabled,
+  onClick,
+  buttonText,
+  description,
+}: IconTextButtonProps) {
+  return (
+    <button
+      className={`
+        flex flex-col items-center rounded-sm border p-5.5 flex-1
+        ${
+          isSelected
+            ? 'border-brand-blue-600 bg-brand-blue-50 text-brand-blue-700'
+            : 'border-brand-gray-200 text-brand-gray-600'
+        }
+      `}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <Icon
+        name={iconName}
+        size={24}
+      />
+      <span className='text-typo-base mt-1'>{buttonText}</span>
+      <p className='text-typo-caption text-brand-gray-500 font-light'>{description}</p>
+    </button>
+  );
+}
