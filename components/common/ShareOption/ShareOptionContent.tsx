87561020fc2f3bd2fc3989c73a738c31dddc828a
JOEIH <joh73459@gmail.com>
2026-07-17 18:22:30 +0900

    feat: 공유 옵션 모달 컴포넌트인 ShareOptionContent 생성
    - 모달 오버레이 및 모달 박스를 제외한, 모달 내부에 렌더링될 부분
    - useSession을 통해 유저 아이디를 불러오고, 현재 유저가 master인지 여부를 판단
    - 보기용/수정용 초대 버튼 및 비공개 설정 버튼, 참여 유저 목록 컴포넌트를 렌더링

---
+++ components/common/ShareOption/ShareOptionContent.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { ResourceParams } from '@/types/shareOption';
+import { Icon } from '../Icon';
+import { MemberList } from './MemberList';
+import { useGetMembers } from '@/hooks/shareOption/useGetMembers';
+import { useModalStore } from '@/lib/store/modalStore';
+import { useShareEditLink } from '@/hooks/invite-member/useShareEditLink';
+import { createViewLink } from '@/lib/utils/invite/createViewLink';
+import { PublicStateSelector } from './PublicStateSelector';
+import { useSession } from 'next-auth/react';
+import { useConfirmDeleteMember } from '@/hooks/shareOption/useConfirmDeleteMember';
+
+export default function ShareOptionContent({ id, resourceType }: ResourceParams) {
+  const { open } = useModalStore();
+  const { createShareLink, isPending } = useShareEditLink();
+  const { data: members } = useGetMembers({ id, resourceType });
+  const { confirmDeleteMember } = useConfirmDeleteMember({ id, resourceType });
+  const { data: session } = useSession();
+
+  // 역할 판단
+  const myRole = members.find((data) => data.id === session?.user.id)?.role;
+  const isMaster = myRole === 'master';
+
+  return (
+    <div className='w-full flex flex-col gap-10 flex-1 min-h-0 overflow-y-auto no-scrollbar'>
+      {/* 수정 권한 초대 버튼 */}
+      <div className='flex flex-col gap-3'>
+        <span className='text-typo-base font-light text-brand-gray-600'>공유 링크</span>
+        <button
+          className='flex items-center gap-2 rounded-sm border border-brand-gray-300 px-4 py-1.5 max-w-60.75'
+          disabled={isPending}
+          onClick={() => createShareLink(id, resourceType)}
+        >
+          <Icon
+            name='UserPlus'
+            size={24}
+            className='text-brand-blue-700'
+          />
+          <span className='text-typo-base text-brand-blue-700'>수정 권한이 있는 사용자 초대</span>
+        </button>
+      </div>
+
+      {/* 공개/비공개 설정 버튼 */}
+      <div className='flex flex-col gap-3'>
+        <span className='text-typo-base font-light text-brand-gray-600'>공개 여부</span>
+        <PublicStateSelector
+          id={id}
+          resourceType={resourceType}
+          disabled={!isMaster}
+        />
+        {/* 보기 링크 공유 버튼 */}
+        <button
+          className='flex self-start items-center justify-center gap-2 rounded-sm border border-brand-gray-300 px-4 py-1.5 w-fit'
+          onClick={() => {
+            open({ type: 'shareLink', props: { type: 'VIEW', link: createViewLink(id, resourceType) } });
+          }}
+        >
+          <Icon
+            name='LinkThin'
+            size={24}
+            className='text-brand-blue-700'
+          />
+          <span className='text-typo-base text-brand-blue-700'>{`${resourceType === 'plan' ? '계획을' : '리스트를'} 보기 위한 링크 복사하기`}</span>
+        </button>
+      </div>
+
+      {/* 참여 유저 목록 */}
+      <MemberList
+        members={members}
+        onRemove={confirmDeleteMember}
+        onInvite={() => createShareLink(id, resourceType)}
+        isMaster={isMaster}
+      />
+    </div>
+  );
+}
