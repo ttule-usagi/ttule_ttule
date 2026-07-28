b6231d50a91e040502fffb647433d4c523db1afc
JOEIH <joh73459@gmail.com>
2026-07-29 01:26:44 +0900

    feat: 장소 이미지 없을 때 이미지 추가

--- components/features/Place/detail/info/PlaceImage.tsx
+++ components/features/Place/detail/info/PlaceImage.tsx
@@ -10,16 +10,22 @@ export default function PlaceImage({ images, koreanName }: PlaceImageProps) {
   const mainImage = images.find((img) => img.isMain) ?? images[0];
 
   return (
-    <div className='h-60 w-full'>
+    <div className='h-60 w-full bg-brand-gray-100 flex flex-col items-center justify-center'>
       {mainImage ? (
         <Image
           src={mainImage.imgUrl}
           alt={koreanName}
           fill
-          className='object-cover'
+          className='object-cover w-full h-full'
         />
       ) : (
-        <div className='w-full h-full bg-brand-gray-100 rounded-lg' />
+        <Image
+          src={'/images/not-found-big.png'}
+          alt='not-found'
+          width={360}
+          height={240}
+          className='w-full h-full object-cover'
+        />
       )}
     </div>
   );
