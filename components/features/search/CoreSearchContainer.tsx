9d4d72e261702f7c25b15884a5481bcf6288a0dc
JOEIH <joh73459@gmail.com>
2026-07-31 19:23:28 +0900

    chore: 검색 결과에서 장소 상세페이지 모달 여는 로직 주석으로 추가

--- components/features/search/CoreSearchContainer.tsx
+++ components/features/search/CoreSearchContainer.tsx
@@ -2,9 +2,11 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import CoreSearchResultList from './CoreSearchResultList';
-import GoogleMapEmbed from '@/components/features/map/GoogleMapEmbed';
+
 import { QueryBoundary } from '@/components/common/ui/boundary/Queryboundary';
+import GoogleMapEmbed from '@/components/features/map/GoogleMapEmbed';
+
+import CoreSearchResultList from './CoreSearchResultList';
 
 interface CoreSearchContainerProps {
   keyword: string;
@@ -12,30 +14,49 @@ interface CoreSearchContainerProps {
 
 export default function CoreSearchContainer({ keyword }: CoreSearchContainerProps) {
   const [selectedPlaceId, setSelectedPlaceId] = useState<string | null>(null);
+  // const { isOpenPlaceModal, selectedId, handleClickPlaceItem, handleClosePlaceDetailModal } = useOpenPlaceDetailModal();
 
   useEffect(() => {
     setSelectedPlaceId(null);
   }, [keyword]);
 
   return (
-    <div className=' h-screen'>
-      {/* 지도 */}
-      <div className='absolute inset-0 ml-118'>
-        <GoogleMapEmbed
-          mode={selectedPlaceId ? 'place' : 'view'}
-          googlePlaceId={selectedPlaceId ?? undefined}
-        />
-      </div>
-
-      {/* 검색 결과 패널 */}
-      <div className='w-full flex flex-col'>
-        <QueryBoundary>
-          <CoreSearchResultList
-            keyword={keyword}
-            onPlaceSelect={(id) => setSelectedPlaceId(id)}
+    <>
+      <div className=' h-screen'>
+        {/* 지도 */}
+        <div className='absolute inset-0 ml-118'>
+          <GoogleMapEmbed
+            mode={selectedPlaceId ? 'place' : 'view'}
+            googlePlaceId={selectedPlaceId ?? undefined}
           />
-        </QueryBoundary>
+        </div>
+
+        {/* 검색 결과 패널 */}
+        <div className='w-full flex flex-col'>
+          <QueryBoundary>
+            <CoreSearchResultList
+              keyword={keyword}
+              onPlaceSelect={(id) => {
+                setSelectedPlaceId(id);
+                // handleClickPlaceItem(id); // corePlaceId 필요
+              }}
+            />
+          </QueryBoundary>
+        </div>
       </div>
-    </div>
+      {/* {isOpenPlaceModal &&
+        selectedId &&
+        createPortal(
+          <div className='absolute left-120 w-90 rounded-lg overflow-y-auto max-h-[90vh] top-1/2 -translate-y-1/2 overscroll-contain'>
+            <QueryBoundary>
+              <CorePlaceDetailContainer
+                placeId={selectedId}
+                onClose={handleClosePlaceDetailModal}
+              />
+            </QueryBoundary>
+          </div>,
+          document.body,
+        )} */}
+    </>
   );
 }
