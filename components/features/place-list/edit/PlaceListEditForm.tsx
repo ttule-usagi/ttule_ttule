944c1f8b6c4b2711b152778d59c38845014e4fc5
JOEIH <joh73459@gmail.com>
2026-08-04 02:21:11 +0900

    feat: 장소 리스트 편집용 클라이언트 컴포넌트 작성

---
+++ components/features/place-list/edit/PlaceListEditForm.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import { useConfirmDeletePlace } from '@/hooks/place-list/useConfirmDeletePlace';
+import { useUpdatePlaceList } from '@/hooks/place-list/useUpdatePlaceList';
+import { IconType } from '@/lib/emoji';
+import { EditablePlace, EditablePlaceParams, PlaceListDetail } from '@/types/placeList';
+
+import EditableOverviewField, { PlaceListErrorType } from '../EditableOverviewField';
+
+import EditPlace from './EditPlace';
+
+interface PlaceListEditFormProps {
+  listId: string;
+  initialDetail: PlaceListDetail;
+  initialIcon?: IconType | null;
+  initialPlaces: EditablePlace[];
+}
+
+export default function PlaceListEditForm({
+  listId,
+  initialDetail,
+  initialPlaces,
+  initialIcon,
+}: PlaceListEditFormProps) {
+  const router = useRouter();
+  const [title, setTitle] = useState<string>(initialDetail.title);
+  const [description, setDescription] = useState<string>(initialDetail.description);
+  const [selectIcon, setSelectedIcon] = useState<IconType | undefined | null>(initialIcon);
+  const [places, setPlaces] = useState<EditablePlace[]>(initialPlaces);
+  const [error, setError] = useState<PlaceListErrorType | null>(null);
+  const { mutateAsync: updatePlaceList } = useUpdatePlaceList(listId);
+  const { confirmDeletePlaceList } = useConfirmDeletePlace(listId, (placeId) => {
+    setPlaces((prev) => prev.filter((p) => p.id !== placeId));
+  });
+
+  const handlePlaceMemoChange = ({ id, memoContent }: EditablePlaceParams) => {
+    setPlaces((prev) => prev.map((p) => (p.id === id ? { ...p, memoContent } : p)));
+  };
+
+  const handleSave = async () => {
+    if (!title.trim()) {
+      setError({ type: 'FIELD', message: '리스트 제목을 입력해주세요.' });
+      return;
+    }
+
+    if (title.length > 20) {
+      setError({ type: 'FIELD', message: '리스트 제목은 20자 이내여야 합니다.' });
+      return;
+    }
+
+    try {
+      await updatePlaceList({
+        listId: listId,
+        newTitle: title,
+        newIcon: selectIcon?.emoji || null,
+        newDescription: description.trim() || null,
+        places: places.map((p) => ({ id: p.id, memoContent: p.memoContent })),
+      });
+      router.back();
+    } catch (error) {
+      console.error('장소 리스트 편집 실패', error);
+      setError({ type: 'UPLOAD', message: '장소 리스트 편집에 실패했습니다. 잠시 후 다시 시도해주세요.' });
+    }
+  };
+
+  return (
+    <div className='flex flex-col gap-6 pb-12'>
+      <header className='flex items-center'>
+        <p className='flex-1 text-typo-big-title text-brand-blue-700'>장소 리스트 관리</p>
+        <button
+          onClick={handleSave}
+          className='rounded-lg box-border font-light px-3 py-2 text-brand-gray-0 bg-brand-blue-700 flex items-center justify-center cursor-pointer'
+        >
+          저장하기
+        </button>
+      </header>
+
+      {/* 리스트 개요 */}
+      <EditableOverviewField
+        title={title}
+        onTitleChange={setTitle}
+        description={description}
+        onDescriptionChange={setDescription}
+        icon={selectIcon}
+        onSelectIcon={setSelectedIcon}
+        error={error}
+      />
+
+      {/* 저장된 장소 */}
+      <div className='flex flex-col gap-4 mt-6'>
+        {places.map((p) => (
+          <EditPlace
+            key={p.id}
+            place={p}
+            onMemoChange={handlePlaceMemoChange}
+            onDeletePlace={confirmDeletePlaceList}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
