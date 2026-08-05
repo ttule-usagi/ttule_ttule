056f6574bd500914763fc54db566c109e76bc2e9
JOEIH <joh73459@gmail.com>
2026-08-05 22:02:58 +0900

    design: 지난 여행이 없을 때 문구 수정

--- components/features/plan/lobby/LastTripList.tsx
+++ components/features/plan/lobby/LastTripList.tsx
@@ -15,7 +15,11 @@ export default function LastTripList({ plans }: { plans: PlanOverview[] }) {
           ))}
         </div>
       ) : (
-        <div className='w-full'>일정이 끝난 여행이 존재하지 않습니다.</div>
+        <div className='w-full pt-4 text-brand-gray-400'>
+          아직 지난 여행이 없습니다.
+          <br />
+          여행 일정이 종료되면 이곳에서 다시 확인할 수 있습니다.
+        </div>
       )}
     </>
   );
