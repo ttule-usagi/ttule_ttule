b91be427423d452436fad68dd97f30ca83ef102b
JOEIH <joh73459@gmail.com>
2026-08-06 17:31:57 +0900

    feat: 현재 여행 섹션 추가 및 수정된 컴포넌트, 훅 적용

--- components/features/plan/lobby/LobbyPlanSection.tsx
+++ components/features/plan/lobby/LobbyPlanSection.tsx
@@ -1,38 +1,22 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { useMemo } from 'react';
 
+import { useGetPlanStatus } from '@/hooks/plan/useGetPlanStatus';
 import { useGetAllUserPlans } from '@/hooks/plan/useGetUserPlans';
-import { getPlanStatus } from '@/lib/utils/getPlanStatus';
-import { PlanOverview } from '@/types/plan';
 
 import JoinPlanButton from '../../JoinPlanButton';
 
-import LastTripList from './LastTripList';
-import UpcomingTripList from './UpcomingTripList';
+import LastPlanList from './LastPlanList';
+import PlanListWithTag from './PlanListWithTag';
+import SectionHeader from './SectionHeader';
 
 export default function LobbyPlanSection() {
   const { data } = useGetAllUserPlans();
   const session = useSession();
 
   // 여행 상태에 따라 분류
-  const { upcoming: upcomingPlans, last: lastPlans } = useMemo(
-    () =>
-      (data ?? []).reduce(
-        (acc, plan) => {
-          const status = getPlanStatus({
-            departure: plan.departureDate,
-            arrival: plan.arrivalDate,
-            isDateUndecided: plan.isDateUndecided,
-          });
-          acc[status].push(plan);
-          return acc;
-        },
-        { upcoming: [], last: [] } as Record<'upcoming' | 'last', PlanOverview[]>,
-      ),
-    [data],
-  );
+  const { currentPlans, upcomingPlans, lastPlans } = useGetPlanStatus(data);
 
   return (
     <>
@@ -52,21 +36,18 @@ export default function LobbyPlanSection() {
       </header>
       <main className='mt-22.75 flex flex-col gap-19.5'>
         <div className='flex flex-col gap-6'>
-          <div className='flex items-center gap-4.5 text-typo-title text-brand-blue-800 font-medium'>
-            <div className='w-2.5 h-2.5 bg-brand-blue-700'></div>
-            다가오는 여행
-          </div>
-          <div className='grid grid-cols-[repeat(auto-fill,275.76px)] gap-10.75'>
-            <UpcomingTripList plans={upcomingPlans} />
-          </div>
+          <SectionHeader title='현재 여행' />
+          <PlanListWithTag plans={currentPlans} />
         </div>
 
         <div className='flex flex-col gap-6'>
-          <div className='flex items-center gap-4.5 text-typo-title text-brand-blue-800 font-medium'>
-            <div className='w-2.5 h-2.5 bg-brand-blue-700'></div>
-            지난 여행
-          </div>
-          <LastTripList plans={lastPlans} />
+          <SectionHeader title='다가오는 여행' />
+          <PlanListWithTag plans={upcomingPlans} />
+        </div>
+
+        <div className='flex flex-col gap-6'>
+          <SectionHeader title='지난 여행' />
+          <LastPlanList plans={lastPlans} />
         </div>
       </main>
     </>
