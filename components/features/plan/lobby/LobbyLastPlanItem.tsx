3d1c45cd872b60dbe52ebe1fd4a84344a2aa05cf
JOEIH <joh73459@gmail.com>
2026-08-09 04:33:31 +0900

    design: 지난 여행 계획 아이템 디자인 조정
    - title이 늘어날 것을 대비해 flex 컨테이너로 변경
    - 제목을 3줄까지만 보여주고 말줄임 처리

--- components/features/plan/lobby/LobbyLastPlanItem.tsx
+++ components/features/plan/lobby/LobbyLastPlanItem.tsx
@@ -18,7 +18,7 @@ export default function LobbyLastPlanItem({
 }: PlanOverview) {
   return (
     <Link
-      className="bg-[url('/images/lobby-plan-folder.svg')] bg-cover bg-center] min-w-85 aspect-340/252 px-7"
+      className="bg-[url('/images/lobby-plan-folder.svg')] bg-cover bg-center min-w-85 aspect-340/252 px-7 flex flex-col"
       href={`/plan/${id}`}
     >
       {/* 여행지 */}
@@ -42,32 +42,36 @@ export default function LobbyLastPlanItem({
       </div>
 
       {/* 계획명 */}
-      <p className='text-typo-title text-brand-blue-700 mt-1'>{title ? title : 'Untitled'}</p>
+      <div className='flex flex-col gap-4 flex-1 min-h-0'>
+        <p className='text-typo-title text-brand-blue-700 mt-1 flex-1 min-h-0 line-clamp-3 overflow-hidden'>
+          {title ? title : 'Untitled'}
+        </p>
 
-      <div className='flex flex-col items-start text-typo-base font-light text-brand-gray-700 mt-19.25 w-full'>
-        {/* 여행 기간 */}
-        <div className='flex items-center justify-center gap-2.5 w-full'>
-          <Icon
-            name='Calendar'
-            size={16}
-          />
-          <div className='flex flex-1 items-center justify-start gap-2'>
-            <span>{departureDate}</span>
+        <div className='flex flex-col items-start text-typo-base font-light text-brand-gray-700 w-full self-end mb-6 shrink-0'>
+          {/* 여행 기간 */}
+          <div className='flex items-center justify-center gap-2.5 w-full'>
             <Icon
-              name='ArrowRight'
+              name='Calendar'
               size={16}
             />
-            <span>{arrivalDate}</span>
+            <div className='flex flex-1 items-center justify-start gap-2'>
+              <span>{departureDate}</span>
+              <Icon
+                name='ArrowRight'
+                size={16}
+              />
+              <span>{arrivalDate}</span>
+            </div>
           </div>
-        </div>
 
-        {/* 업데이트 시간 */}
-        <div className='flex items-center justify-center gap-2.5 w-full'>
-          <Icon
-            name='Clock'
-            size={16}
-          />
-          <span className='flex-1'>{formatRelativeDate(updatedAt)}</span>
+          {/* 업데이트 시간 */}
+          <div className='flex items-center justify-center gap-2.5 w-full'>
+            <Icon
+              name='Clock'
+              size={16}
+            />
+            <span className='flex-1'>{formatRelativeDate(updatedAt)}</span>
+          </div>
         </div>
       </div>
     </Link>
