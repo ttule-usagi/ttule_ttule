a88731eddb294ca0b1046831379fa2ec2dde7c30
JOEIH <joh73459@gmail.com>
2026-08-12 03:52:06 +0900

    refactor: 사이드바 프로필 조회에 prefetch 로직 적용
    - session이 있을 때만 유저 정보 prefetch
    - 프로필 조회 시 에러가 발생하면 default 이미지 노출 설정

--- components/features/ProfileImage.tsx
+++ components/features/ProfileImage.tsx
@@ -1,13 +1,14 @@
-import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 
+import { useGetUserInfo } from '@/hooks/user/useGetUserInfo';
+
 export default function ProfileImage() {
-  const { data: session } = useSession();
+  const { data: user } = useGetUserInfo();
   return (
     <div className='w-12 h-12 rounded-full bg-brand-blue-100 box-border border border-brand-blue-700'>
-      {session?.user?.image && (
+      {user.profileImageUrl && (
         <Image
-          src={session.user.image}
+          src={user.profileImageUrl}
           alt='profile image'
           width={48}
           height={48}
--- components/layouts/sidebar/Sidebar.tsx
+++ components/layouts/sidebar/Sidebar.tsx
@@ -1,16 +1,27 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import LoginButtonProfile from '@/components/common/LoginButtonProfile';
+import { prefetchUserInfo } from '@/lib/actions/api/prefetch/prefetchUserInfo';
 import { auth } from '@/lib/utils/auth';
+import { getQueryClient } from '@/lib/utils/getQueryClient';
+
 import SidebarNav from './SidebarNav';
 import SidebarProfile from './SidebarProfile';
-import LoginButtonProfile from '@/components/common/LoginButtonProfile';
 
 export default async function Sidebar() {
   const session = await auth();
+  const queryClient = getQueryClient();
+
+  if (session) {
+    await prefetchUserInfo(queryClient);
+  }
 
   return (
     <div className='fixed h-full max-w-16 bg-brand-blue-700 flex flex-col justify-center items-center z-999'>
       <SidebarNav />
-      {session && <SidebarProfile />}
-      {!session && <LoginButtonProfile />}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        {session ? <SidebarProfile /> : <LoginButtonProfile />}
+      </HydrationBoundary>
     </div>
   );
 }
