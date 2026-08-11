76a085c50b1fe3b4245f2b861839b342aee14f09
JOEIH <joh73459@gmail.com>
2026-08-12 03:07:53 +0900

    feat: 유저 프로필 조회 서버 컴포넌트 로직 작성

--- app/(user)/mypage/page.tsx
+++ app/(user)/mypage/page.tsx
@@ -1,3 +1,22 @@
-export default function MyPage() {
-  return <div>내 정보 관리 페이지</div>;
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import NotePage from '@/components/common/NotePage';
+import { QueryBoundary } from '@/components/common/ui/boundary/Queryboundary';
+import EditInfoForm from '@/components/features/user/EditInfoForm';
+import { prefetchUserInfo } from '@/lib/actions/api/prefetch/prefetchUserInfo';
+import { getQueryClient } from '@/lib/utils/getQueryClient';
+
+export default async function MyPage() {
+  const queryClient = getQueryClient();
+  await prefetchUserInfo(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePage title='내 정보'>
+        <QueryBoundary subject='내 정보'>
+          <EditInfoForm />
+        </QueryBoundary>
+      </NotePage>
+    </HydrationBoundary>
+  );
 }
