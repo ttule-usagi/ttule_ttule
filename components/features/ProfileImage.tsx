6fee108dd6b2db34277a650e8276274244924b82
JOEIH <joh73459@gmail.com>
2026-08-12 03:55:13 +0900

    feat: 이미지 지정하지 않았을 경우 사이드바 프로필 이미지에 default 이미지 노출 설정

--- components/features/ProfileImage.tsx
+++ components/features/ProfileImage.tsx
@@ -1,20 +1,19 @@
 import Image from 'next/image';
 
 import { useGetUserInfo } from '@/hooks/user/useGetUserInfo';
+import { DEFAULT_PROFILE_IMAGE } from '@/lib/constants/image';
 
 export default function ProfileImage() {
   const { data: user } = useGetUserInfo();
   return (
     <div className='w-12 h-12 rounded-full bg-brand-blue-100 box-border border border-brand-blue-700'>
-      {user.profileImageUrl && (
-        <Image
-          src={user.profileImageUrl}
-          alt='profile image'
-          width={48}
-          height={48}
-          className='w-full h-full rounded-full object-cover'
-        />
-      )}
+      <Image
+        src={user.profileImageUrl || DEFAULT_PROFILE_IMAGE}
+        alt='profile image'
+        width={48}
+        height={48}
+        className='w-full h-full rounded-full object-cover'
+      />
     </div>
   );
 }
