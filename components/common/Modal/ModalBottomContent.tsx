da4db057448de951a9aa82bc79e7c7037d50dc37
JOEIH <joh73459@gmail.com>
2026-08-18 20:39:27 +0900

    refactor: ModalBottomContent에서 다양한 스타일을 받을 수 있게 수정

--- components/common/Modal/ModalBottomContent.tsx
+++ components/common/Modal/ModalBottomContent.tsx
@@ -1,7 +1,8 @@
 interface ModalBottomContentProps {
   children: React.ReactNode;
+  classname?: string;
 }
 
-export default function ModalBottomContent({ children }: ModalBottomContentProps) {
-  return <div className='flex justify-center items-center gap-2.5'>{children}</div>;
+export default function ModalBottomContent({ children, classname }: ModalBottomContentProps) {
+  return <div className={classname || 'flex justify-center items-center gap-2.5'}>{children}</div>;
 }
